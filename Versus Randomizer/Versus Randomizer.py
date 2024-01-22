494ee2e26ef4ebc4216e942a0a763d1d95f1b778
Gerardo Ruiz <grville93@gmail.com>
2024-01-22 13:51:25 -0500

    Rearrangement of the code and documentation added

--- Versus Randomizer/Versus Randomizer.py
+++ Versus Randomizer/Versus Randomizer.py
@@ -1,34 +1,30 @@
-"Let's create a versus randomizer"
-
 import random
 import time
 
+# Competitors population
 competitors = list()
-no_comp = int(input('Enter the number of competitors: \n'))
 
+no_comp = int(input('Enter the number of competitors: '))
 
 for comp in range(no_comp):
-
     competitors.append(input(f'Enter the name of the # {comp+1} competitor: '))
 
 
+# Creating and shuffling the positions to later pair the matches
 positions = list(range(no_comp))
-
 random.shuffle(positions)
 
-matches = list()
 
-print()
-
-print("Randomizing... Get Ready!\nIn...")
+# Signaling the user the matches pairing is about to start
+print("\nRandomizing... Get Ready!\nIn...")
 
 for i in range(3,0,-1):
     print(f'{i}')
     time.sleep(1)
 
-print()
 
-print('Here are the matches:\n')
+# Outputting the results
+print('\nHere are the matches:\n')
 
 for match in range(no_comp//2):
     print(f'Match # {match+1}: {competitors[ positions[(2*match)] ]} vs {competitors[ positions[(2*match+1)] ]}')
